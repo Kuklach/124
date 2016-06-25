0466c897ab21a0b2d729a790963f601b20e2474a
Hanter <Leo.Novikov1996@yandex.ru>
2016-06-26 00:53:02 +0300

    fix 3 + codestyle
    Add server log??, add target to prop freeze, add valid chek for player

--- ulx/lua/ulx/modules/sh/ADVcomman.lua
+++ ulx/lua/ulx/modules/sh/ADVcomman.lua
@@ -1,6 +1,9 @@
 local CATEGORY_NAME = "Utility"
 
-function ulx.nolag(ply)
+
+function ulx.nolag(ply,targ)
+
+	if (not ply:IsValid()) then return end
 
 	for _, v in pairs( ents.FindByClass( "prop_*" ) ) do
 
@@ -8,16 +11,34 @@ function ulx.nolag(ply)
 
 		if IsValid( phys ) then
 
-			phys:EnableMotion( false )
+			if (targ:IsValid() and v:GetOwner() == v) then
+
+				phys:EnableMotion( false )
+
+			else
+
+				phys:EnableMotion( false )
+
+			end
 
 		end
 
 	end
 
-	ULib.tsayColor(	_,true,Color(0, 43, 54),"All props has been freezed!!!")
+	if (SERVER) then
+
+		ULib.console(_,ply:Name() .. " : Call [ulx nolag]")
+
+	else
+
+		ULib.tsayColor(_,true,Color(0, 93, 104),"All props has been freezed!!!")
+
+	end
 
 end
 
+
 local nolag = ulx.command( CATEGORY_NAME, "ulx nolag", ulx.nolag, "!nolag")
+nolag:addParam{ type = ULib.cmds.PlayerArg, target = "!%superadmin", default = "^", ULib.cmds.optional }
 nolag:help( "Freeze all props!" )
 nolag:defaultAccess( ULib.ACCESS_ADMIN )
