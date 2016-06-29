763aaa08f47de1bac0bb7923b03b3c8776653b8f
Hanter <Leo.Novikov1996@yandex.ru>
2016-06-30 00:30:37 +0300

    wait for update

--- ulx/lua/ulx/modules/sh/ADVcomman.lua
+++ ulx/lua/ulx/modules/sh/ADVcomman.lua
@@ -3,42 +3,28 @@ local CATEGORY_NAME = "Utility"
 
 function ulx.nolag(calling_ply,target_ply)
 
-	local pl = target_ply:IsValid()
-
 	for _, v in pairs( ents.FindByClass( "prop_*" ) ) do
 
 		local phys = v:GetPhysicsObject()
 
 		if phys:IsValid() then
 
-			if (pl == true) then
-
-				if (v:GetOwner() == target_ply) then
-
-					phys:EnableMotion( false )
-
-				end
-
-			else
-
 				phys:EnableMotion( false )
-
-			end
 
 		end
 
 	end
 
-	 if (pl == true) then
-		 ulx.fancyLogAdmin( calling_ply, "#A frozen #T props!", target_ply )
-	 else
+	 //if (not pl) then
+		 //ulx.fancyLogAdmin( calling_ply, "#A frozen #T props!", target_ply )
+	 //else
 		 ulx.fancyLogAdmin( calling_ply, "#A froze all the props!")
-	 end
+	 //end
 
 end
 
 
 local nolag = ulx.command( CATEGORY_NAME, "ulx nolag", ulx.nolag, "!nolag")
-nolag:addParam{ type = ULib.cmds.PlayerArg, ULib.cmds.optional }
+//nolag:addParam{ type = ULib.cmds.PlayerArg, hint="player",ULib.cmds.optional}
 nolag:defaultAccess( ULib.ACCESS_ADMIN )
 nolag:help( "Freeze all props!" )
