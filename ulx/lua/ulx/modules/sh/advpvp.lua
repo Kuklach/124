ef4d239db8132c9db73cf2211ba073f33da927c9
Hanter <Leo.Novikov1996@yandex.ru>
2016-07-15 01:42:58 +0300

    fix! compile to 1 command

--- ulx/lua/ulx/modules/sh/advpvp.lua
+++ ulx/lua/ulx/modules/sh/advpvp.lua
@@ -1,58 +1,57 @@
 local CATEGORY_NAME = "Fun"
------------------------------- PvP ------------------------------
-function ulx.pvp(calling_ply)
+------------------------------ PvP & Build ------------------------------
+function ulx.pvp(calling_ply,OnBuild)
 
-	if (calling_ply.ULXHasGod) then
+	if (OnBuild) then
 
-		calling_ply:Spawn()
-		calling_ply:GodDisable()
-		calling_ply.ULXHasGod = nil
-		hook.Run("PlayerLoadout",calling_ply)
-		ulx.fancyLogAdmin( calling_ply, "#A changed mod to PvP!" )
+		if (calling_ply.restrictBuild == true) then
+
+			ULib.tsayError( calling_ply, "Охуевшее тело тебе запрещен Build мод!", true )
+			calling_ply:KillSilent()
+			return
+
+		elseif (timer.Exists(calling_ply:Name())) then
+
+			timer.Remove(calling_ply:Name())
+			calling_ply:SetHealth(100)
+
+		end
+
+		if (calling_ply.ULXHasGod) then
+
+			ULib.tsayError( calling_ply, "You arleady in Build mode!", true )
+
+		else
+
+			calling_ply:GodEnable()
+			calling_ply.ULXHasGod = true
+			hook.Run("PlayerLoadout",calling_ply)
+			ulx.fancyLogAdmin( calling_ply, "#A changed mod to Build!" )
+
+		end
 
 	else
 
-		ULib.tsayError( calling_ply, "You arleady in PvP mode!", true )
+		if (calling_ply.ULXHasGod) then
+
+			calling_ply:Spawn()
+			calling_ply:GodDisable()
+			calling_ply.ULXHasGod = nil
+			hook.Run("PlayerLoadout",calling_ply)
+			ulx.fancyLogAdmin( calling_ply, "#A changed mod to PvP!" )
+
+		else
+
+			ULib.tsayError( calling_ply, "You arleady in PvP mode!", true )
+
+		end
 
 	end
 
 end
 
 local pvp = ulx.command( CATEGORY_NAME, "ulx pvp", ulx.pvp, "!pvp")
+pvp:addParam{ type = ULib.cmds.BoolArg, invisible = true }
 pvp:defaultAccess( ULib.ACCESS_ALL)
 pvp:help( "PvP mod!" )
------------------------------- Build ------------------------------
-function ulx.build(calling_ply)
-
-	if (calling_ply.restrictBuild == true) then
-
-		ULib.tsayError( calling_ply, "Охуевшее тело тебе запрещен Build мод!", true )
-		calling_ply:KillSilent()
-		return
-
-	elseif (timer.Exists(calling_ply:Name())) then
-
-		timer.Remove(calling_ply:Name())
-		calling_ply:SetHealth(100)
-
-	end
-
-	if (calling_ply.ULXHasGod) then
-
-		ULib.tsayError( calling_ply, "You arleady in Build mode!", true )
-
-	else
-
-		calling_ply:GodEnable()
-		calling_ply.ULXHasGod = true
-		hook.Run("PlayerLoadout",calling_ply)
-		ulx.fancyLogAdmin( calling_ply, "#A changed mod to Build!" )
-
-	end
-
-end
-
-
-local build = ulx.command( CATEGORY_NAME, "ulx build", ulx.build, "!build")
-build:defaultAccess( ULib.ACCESS_ALL)
-build:help( "Build mod!" )
+pvp:setOpposite( "ulx build", {_, true}, "!build" )
